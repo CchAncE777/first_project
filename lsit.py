6fb35b1252d105301b15e9786c9074ae02ecf6eb
CchAncE777 <chance7.melol@gmail.com>
2025-10-27 15:30:25 +0300

    f

--- lsit.py
+++ lsit.py
@@ -1,18 +1,27 @@
 l = [3, 6, 9, 11, 17, 29, 33, 52, 69]
 a = len(l)
 b = int(a**(1/2))
-iskomoe = 69
+iskomoe = 3
 index = 0
+is_found = False
+
 for i  in range(0, a, b):
     if iskomoe <l[i]:
         for j in range(i-a, i):
             if l[j] == iskomoe:
                 index = j
+                is_found = True
                 break
     if l[index] == iskomoe:
          break
 for j in range(i, len(l)):
             if l[j] == iskomoe:
                 index = j
+                is_found = True
                 break
 print(index)
+
+if is_found:
+    print(index)
+else:
+     print('Not found')
