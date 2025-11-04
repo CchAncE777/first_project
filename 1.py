3536b89e341d90a7e370a361b2eee7ed43228bbc
CchAncE777 <chance7.melol@gmail.com>
2025-11-04 13:29:02 +0300

    g

--- 1.py
+++ 1.py
@@ -9,3 +9,13 @@ for item in text:
         words.update({item:1})
 
 print(words)
+
+f = open('text.txt', 'r', encoding='UTF-8').read()
+f = f.lower()
+text = f.split()
+text
+
+letters = {}
+
+print(ord('А'))
+print(ord('я'))
